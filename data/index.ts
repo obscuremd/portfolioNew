2b27871c549a3e6aa96252a61da92458db752412
dan <md.erhenede@gmail.com>
2024-06-01 20:15:37 +0100

    pp

--- data/index.ts
+++ data/index.ts
@@ -73,7 +73,7 @@ export const projects = [
   {
     id: 1,
     title: "RWA project",
-    des: "Recreated the Apple iPhone 15 Pro website, combining GSAP animations and Three.js 3D effects..",
+    des: "This project marked my first professional role, where I applied my years of accumulated experience to a full-stack blockchain initiative. Under my leadership, the company achieved a 35% increase in user traffic and an estimated 57% rise in profits.",
     img: "https://firebasestorage.googleapis.com/v0/b/social-media-fd6de.appspot.com/o/socialMedia%2Fportfolio%2Frwa.png?alt=media&token=54098136-4f3b-471a-839a-199db47f8616",
     iconLists: ["/re.svg", "/tail.svg", "/c.svg", "/fm.svg", "/solid.svg"],
     link: "https://rwa-obscure.netlify.app",
@@ -81,7 +81,7 @@ export const projects = [
   {
     id: 2,
     title: "Tea",
-    des: "Recreated the Apple iPhone 15 Pro website, combining GSAP animations and Three.js 3D effects..",
+    des: "Tea was a passion project I had envisioned since childhood, and I was finally able to bring it to life using the skills I acquired over the years. This full-stack project attracted over 5,000 active daily users.",
     img: "https://firebasestorage.googleapis.com/v0/b/social-media-fd6de.appspot.com/o/socialMedia%2Fportfolio%2FInstagram%20post%20-%201.png?alt=media&token=5b384d16-ad68-4867-93f9-1f68ffa77f56",
     iconLists: ["/next.svg", "/tail.svg", "/fm.svg", "/node.svg", "/mongo.svg"],
     link: "https://social-media-giox.vercel.app",
@@ -89,7 +89,7 @@ export const projects = [
   {
     id: 3,
     title: "Sizzles",
-    des: "A REAL Software-as-a-Service app with AI features and a payments and credits system using the latest tech stack.",
+    des: "Sizzles was an app concept I developed while learning product design and UI/UX design. After mastering app development, I turned that concept into a fully functional application.",
     img: "https://firebasestorage.googleapis.com/v0/b/social-media-fd6de.appspot.com/o/socialMedia%2Fportfolio%2Fpage%2013.png?alt=media&token=f950b38c-9237-4f91-b479-9baed90c5d1a",
     iconLists: [
       "/re.svg",
@@ -103,7 +103,7 @@ export const projects = [
   {
     id: 4,
     title: "Old Portfolio",
-    des: "Recreated the Apple iPhone 15 Pro website, combining GSAP animations and Three.js 3D effects..",
+    des: "This was an old portfolio I created during my early career. I no longer use it because, after collecting and analyzing data with Excel, I identified the reasons for the lack of proposals. I then used these insights to guide a complete redesign of my portfolio.",
     img: "https://firebasestorage.googleapis.com/v0/b/social-media-fd6de.appspot.com/o/socialMedia%2Fportfolio%2Fimage_2024-06-01_175547030.png?alt=media&token=7894cd72-f25d-4089-b8b5-876e974e8589",
     iconLists: [
       "/re.svg",
@@ -117,7 +117,7 @@ export const projects = [
   {
     id: 5,
     title: "Shopify Clone",
-    des: "Explore the wonders of our solar system with this captivating 3D simulation of the planets using Three.js.",
+    des: "The React Shopify app is an e-commerce application built to facilitate online shopping experiences, providing users with a seamless interface to browse products, add them to the cart, and proceed to checkout. The app integrates with Shopify's APIs to fetch product data, manage inventory, and handle orders.",
     img: "https://firebasestorage.googleapis.com/v0/b/social-media-fd6de.appspot.com/o/socialMedia%2Fportfolio%2Fshopify%20card.png?alt=media&token=b4488746-95ee-479f-83c2-789a47c2ac92",
     iconLists: [
       "/re.svg",
@@ -131,7 +131,7 @@ export const projects = [
   {
     id: 6,
     title: "Pokedex App",
-    des: "Simplify your video conferencing experience with Yoom. Seamlessly connect with colleagues and friends.",
+    des: "The Pokédex Web Application serves as a digital encyclopedia of Pokémon species, offering users comprehensive information about various Pokémon characters. Utilizing HTML, CSS, and JavaScript, the application allows users to search for Pokémon by name or ID and access details such as characteristics, types, abilities, and more, all fetched from the PokeAPI.",
     img: "https://firebasestorage.googleapis.com/v0/b/social-media-fd6de.appspot.com/o/socialMedia%2Fportfolio%2Fpokedex%20card.png?alt=media&token=8d71a016-12d9-4fd5-a61b-80af3855fdb5",
     iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/stream.svg", "/c.svg"],
     link: "https://obs-pokedex.netlify.app",
@@ -139,7 +139,7 @@ export const projects = [
   {
     id: 7,
     title: "Movie Website",
-    des: "Recreated the Apple iPhone 15 Pro website, combining GSAP animations and Three.js 3D effects..",
+    des: "The Responsive Movie App is a web application designed to provide users with information about movies, including details such as titles, posters, overviews, ratings, and release dates. Leveraging the TMDB (The Movie Database) API, the app fetches data about movies, enabling users to browse through a vast collection of films and access comprehensive information effortlessly.",
     img: "https://firebasestorage.googleapis.com/v0/b/social-media-fd6de.appspot.com/o/socialMedia%2Fportfolio%2Fmovie%20card.png?alt=media&token=4e211d21-9cd3-4084-aaac-960c023f8e44",
     iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/three.svg", "/gsap.svg"],
     link: "https://obs-movie-app.netlify.app",
@@ -147,7 +147,7 @@ export const projects = [
   {
     id: 8,
     title: "Car Game",
-    des: "Recreated the Apple iPhone 15 Pro website, combining GSAP animations and Three.js 3D effects..",
+    des: "The HTML5 Car Game is a web-based game designed to provide users with an entertaining and immersive experience of driving a car. Leveraging HTML, JavaScript, and CSS, the game creates a dynamic and interactive environment where players can control a car, avoid obstacles, and achieve high scores.",
     img: "https://firebasestorage.googleapis.com/v0/b/social-media-fd6de.appspot.com/o/socialMedia%2Fportfolio%2Fcar%20card.png?alt=media&token=93135bb7-9c77-467a-a73b-d50be77328e4",
     iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/three.svg", "/gsap.svg"],
     link: "https://obscure-car-game.netlify.app",
@@ -155,7 +155,7 @@ export const projects = [
   {
     id: 9,
     title: "Youtube Clone",
-    des: "Recreated the Apple iPhone 15 Pro website, combining GSAP animations and Three.js 3D effects..",
+    des: "The YouTube Clone Web Application is a replica of the popular video-sharing platform YouTube, designed to provide users with a familiar and intuitive interface for browsing and watching videos. Leveraging React for building the user interface, React Router for navigation, and the YouTube API from RapidAPI for fetching video data, the application offers functionalities similar to those found on the original YouTube platform.",
     img: "https://firebasestorage.googleapis.com/v0/b/social-media-fd6de.appspot.com/o/socialMedia%2Fportfolio%2FYoutube%20card.png?alt=media&token=37335147-9731-4e5a-a3d0-83fbf27413b8",
     iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/redux.svg", "/router.svg"],
     link: "https://obs-youtube.netlify.app",
@@ -163,7 +163,7 @@ export const projects = [
   {
     id: 10,
     title: "TradeFinex Clone",
-    des: "Recreated the Apple iPhone 15 Pro website, combining GSAP animations and Three.js 3D effects..",
+    des: "The Finex Blockchain Website Clone is a faithful replica of the original Finex blockchain website, crafted to highlight proficiency in web development and frontend technologies. Employing TypeScript for robust type safety, React Router DOM for seamless navigation, and Tailwind CSS for sleek styling, the clone meticulously emulates the visual layout and functionality of its source, offering a compelling showcase of frontend development expertise.",
     img: "https://firebasestorage.googleapis.com/v0/b/social-media-fd6de.appspot.com/o/socialMedia%2Fportfolio%2FTrade%20card.png?alt=media&token=b1b95230-62f0-4533-8711-b2ddf109a469",
     iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/redux.svg", "/router.svg"],
     link: "https://obs-tradefinex.netlify.app",
@@ -171,7 +171,7 @@ export const projects = [
   {
     id: 11,
     title: "NextJs Crud",
-    des: "Recreated the Apple iPhone 15 Pro website, combining GSAP animations and Three.js 3D effects..",
+    des: "Developed a Next.js full-stack CRUD application with create, update, and delete functionalities, utilizing MongoDB and Next.js routing.",
     img: "https://firebasestorage.googleapis.com/v0/b/social-media-fd6de.appspot.com/o/socialMedia%2Fportfolio%2Fimage_2024-06-01_175256671.png?alt=media&token=bd4062ea-d969-4569-baa8-8e6a18e28973",
     iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/node.svg", "/mongo.svg"],
     link: "https://obs-nextjs-crud.netlify.app",
